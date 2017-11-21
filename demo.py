436ac9c9a6c380d309a1dbd42a301f1c7b57b42a
yogendratomar123 <33837020+yogendratomar123@users.noreply.github.com>
2017-11-21 18:44:41 +0530

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,10 @@
+print("Let's see how long you have lived in days, minutes and seconds.");
+name=raw_input("enter your name: ");
+print("now enter your age");
+age = int(input("age: "));
+print('your age:',age);
+days = age * 365
+minutes = age * 525948     
+seconds = age * 31556926
+print(name, "has been alive for", days,"days", minutes, "minutes and", seconds, "seconds!  Wow!");
+
